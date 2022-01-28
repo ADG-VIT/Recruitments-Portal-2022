cb88ac57e7e2aeb75110b766d54239cefedf2397
anmolbansal7 <abanmolbansal5@gmail.com>
2022-01-28 20:27:01 +0530

    content updates

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -9,107 +9,112 @@ import Input from "../Inputs/Input";
 import Otp from "../Otp/Otp.js";
 
 function Login() {
-  const [isOtp, setIsOtp] = useState(0);
-  const [reg_no, setReg_no] = useState("");
+	const [isOtp, setIsOtp] = useState(0);
+	const [reg_no, setReg_no] = useState("");
 
-  const handleChange1 = (e) => {
-    setReg_no(e.target.value);
-  };
+	const handleChange1 = (e) => {
+		setReg_no(e.target.value);
+	};
 
-  function handleClick() {
-    setIsOtp(1);
-  }
+	function handleClick() {
+		setIsOtp(1);
+	}
 
+	const navigate = useNavigate();
 
-  const navigate = useNavigate();
-
-  return (
-    <>
-      <Navbar navbar={0} />
-      {isOtp ? (
-        <div className="otp_page_login">
-          <div className="left">
-            <div className="main_form">
-              <h1 className="heading">One Time Password</h1>
-              <p className="para">
-                Check your VIT Mail Inbox or Spam Folder for the OTP
-              </p>
-              <form className="form">
-              <p className = "otp_para">OTP</p>
-                <Otp />
-                <p className="bottom">
-                  Didn’t recieve OTP?{" "}
-                  <span className="resendOTP" onClick={() => {}}>
-                    {" "}
-                    Resend OTP{" "}
-                  </span>
-                </p>
-              </form>
-              <Button
-                class="btn1"
-                ClickFunction={() => {
-                  navigate("/aboutyou");
-                }}
-                heading="Verify OTP"
-              />
-              <p
-                className="tosignup"
-                onClick={() => {
-                  setIsOtp(0);
-                }}
-              >
-                Go Back
-              </p>
-            </div>
-          </div>
-          <div className="right">
-            <img alt="background" src={backImg} />
-          </div>
-        </div>
-      ) : (
-        <div className="login_page">
-          <div className="left">
-            <div className="main_form">
-              <h1 className="heading">Login</h1>
-              <p className="para">Login with your VIT Registration Number</p>
-              <form className="form">
-                  <Input
-                    setnull={setReg_no}
-                    val={reg_no}
-                    change={handleChange1}
-                    heading="Registration Number"
-                    placeholder="Enter your Registration Number. Eg: 20BCE0001"
-                    optional =""
-                />
-              </form>
-              <Button
-                class="btn1"
-                ClickFunction={() => {
-                  handleClick();
-                }}
-                heading="Login with OTP"
-              />
-              <p className="bottom">
-                Don’t have an account?{" "}
-                <span
-                  className="tosignup"
-                  onClick={() => {
-                    navigate("/SignUp");
-                  }}
-                >
-                  {" "}
-                  Create One
-                </span>
-              </p>
-            </div>
-          </div>
-          <div className="right">
-            <img alt="background" src={backImg} />
-          </div>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			<Navbar navbar={0} />
+			{isOtp ? (
+				<div className="otp_page_login">
+					<div className="left">
+						<div className="main_form">
+							<h1 className="heading">One Time Password</h1>
+							<p className="para">
+								Check your VIT Mail Inbox or Spam Folder for the
+								OTP
+							</p>
+							<form className="form">
+								<p className="otp_para">OTP</p>
+								<Otp />
+								<p className="bottom">
+									Didn’t receive OTP?{" "}
+									<span
+										className="resendOTP"
+										onClick={() => {}}
+									>
+										{" "}
+										Resend OTP{" "}
+									</span>
+								</p>
+							</form>
+							<Button
+								class="btn1"
+								ClickFunction={() => {
+									navigate("/aboutyou");
+								}}
+								heading="Verify OTP"
+							/>
+							<p
+								className="tosignup"
+								onClick={() => {
+									setIsOtp(0);
+								}}
+							>
+								Go Back
+							</p>
+						</div>
+					</div>
+					<div className="right">
+						<img alt="background" src={backImg} />
+					</div>
+				</div>
+			) : (
+				<div className="login_page">
+					<div className="left">
+						<div className="main_form">
+							<h1 className="heading">Login</h1>
+							<p className="para">
+								Login with your VIT Registration Number
+							</p>
+							<form className="form">
+								<Input
+									setnull={setReg_no}
+									val={reg_no}
+									change={handleChange1}
+									heading="Registration Number"
+									placeholder="Enter your Registration Number. Eg: 21BCE0001"
+									optional=""
+								/>
+							</form>
+							<Button
+								class="btn1"
+								ClickFunction={() => {
+									handleClick();
+								}}
+								heading="Login with OTP"
+							/>
+							<p className="bottom">
+								Don’t have an account yet?{" "}
+								<span
+									className="tosignup"
+									onClick={() => {
+										navigate("/SignUp");
+									}}
+								>
+									{" "}
+									Create One
+								</span>
+							</p>
+						</div>
+					</div>
+					<div className="right">
+						<img alt="background" src={backImg} />
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default Login;
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -9,146 +9,151 @@ import Button from "../Inputs/Button";
 import Input from "../Inputs/Input";
 import Otp from "../Otp/Otp.js";
 
-
 function SignUp() {
-  const navigate = useNavigate();
-  const [isOtp, setIsOtp] = useState(0);
+	const navigate = useNavigate();
+	const [isOtp, setIsOtp] = useState(0);
 
-  const [name, setName] = useState("");
-  const [reg_no, setReg_no] = useState("");
-  const [email, setEmail] = useState("");
-  const [ph, setPh] = useState("");
+	const [name, setName] = useState("");
+	const [reg_no, setReg_no] = useState("");
+	const [email, setEmail] = useState("");
+	const [ph, setPh] = useState("");
 
-  const handleChange1 = (e) => {
-    setName(e.target.value);
-  };
-  const handleChange2 = (e) => {
-    setReg_no(e.target.value);
-  };
-  const handleChange3 = (e) => {
-    setEmail(e.target.value);
-  };
-  const handleChange4 = (e) => {
-    setPh(e.target.value);
-  };
+	const handleChange1 = (e) => {
+		setName(e.target.value);
+	};
+	const handleChange2 = (e) => {
+		setReg_no(e.target.value);
+	};
+	const handleChange3 = (e) => {
+		setEmail(e.target.value);
+	};
+	const handleChange4 = (e) => {
+		setPh(e.target.value);
+	};
 
-  const handleClick = () => {
-    setIsOtp(1);
-  };
+	const handleClick = () => {
+		setIsOtp(1);
+	};
 
-  return (
-    <>
-      <Navbar navbar={0} />
-      {isOtp ? (
-        <div className="otp_page">
-          <div className="left">
-            <img alt="background" src={back_img} />
-          </div>
-          <div className="right">
-            <div className="main_form">
-              <h1 className="heading">Verify your Account</h1>
-              <p className="para">
-                Check your VIT Mail Inbox or Spam Folder for the OTP
-              </p>
-              <form className="form">
-                <p className = "otp_para">OTP</p>
-                <Otp />
-                <p className="bottom">
-                  Didn’t recieve OTP?{" "}
-                  <span className="resendOTP" onClick={() => {}}>
-                    {" "}
-                    Resend OTP{" "}
-                  </span>
-                </p>
-              </form>
-              <Button
-                class="btn1"
-                ClickFunction={() => {
-                  navigate("/aboutyou");
-                }}
-                heading="Verify OTP"
-              />
-              <p
-                className="tosignup"
-                onClick={() => {
-                  setIsOtp(0);
-                }}
-              >
-                Go Back
-              </p>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="signup_page">
-          <div className="left">
-            <img alt="background" src={back_img} />
-          </div>
-          <div className="right">
-            <div className="main_form">
-              <h1 className="heading">Create an Account</h1>
-              <p className="para">Personal information / Contact Details</p>
-              <form className="form">
-                <Input
-                  setnull={setName}
-                  val={name}
-                  change={handleChange1}
-                  heading="Name"
-                  placeholder="Enter the name"
-                  optional =""
-                  type="text"
-                />
-                <Input
-                  setnull={setReg_no}
-                  val={reg_no}
-                  change={handleChange2}
-                  heading="Registration Number"
-                  optional =""
-                  placeholder="Enter the Reg Number"
-                  type="text"
-                />
-                <Input
-                  setnull={setEmail}
-                  val={email}
-                  change={handleChange3}
-                  heading="VIT Email ID"
-                  placeholder="Enter the VIT Email ID"
-                  optional =""
-                  type="email"
-                />
-                <Input
-                  setnull={setPh}
-                  optional =""
-                  val={ph}
-                  change={handleChange4}
-                  heading="Phone Number"
-                  placeholder="Enter the Ph Number with Country Code"
-                  type="tel"
-                />
-              </form>
-              <Button
-                class="btn1"
-                ClickFunction={handleClick}
-                heading="Create an Account"
-              />
-              <p className="bottom">
-                Already Have an Account?{" "}
-                <span
-                  className="tologin"
-                  onClick={() => {
-                    navigate("/Login");
-                  }}
-                >
-                  {" "}
-                  Login
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			<Navbar navbar={0} />
+			{isOtp ? (
+				<div className="otp_page">
+					<div className="left">
+						<img alt="background" src={back_img} />
+					</div>
+					<div className="right">
+						<div className="main_form">
+							<h1 className="heading">Verify your Account</h1>
+							<p className="para">
+								Check your VIT Mail Inbox or Spam Folder for the
+								OTP
+							</p>
+							<form className="form">
+								<p className="otp_para">OTP</p>
+								<Otp />
+								<p className="bottom">
+									Didn’t receive OTP?{" "}
+									<span
+										className="resendOTP"
+										onClick={() => {}}
+									>
+										{" "}
+										Resend OTP{" "}
+									</span>
+								</p>
+							</form>
+							<Button
+								class="btn1"
+								ClickFunction={() => {
+									navigate("/aboutyou");
+								}}
+								heading="Verify OTP"
+							/>
+							<p
+								className="tosignup"
+								onClick={() => {
+									setIsOtp(0);
+								}}
+							>
+								Go Back
+							</p>
+						</div>
+					</div>
+				</div>
+			) : (
+				<div className="signup_page">
+					<div className="left">
+						<img alt="background" src={back_img} />
+					</div>
+					<div className="right">
+						<div className="main_form">
+							<h1 className="heading">Create an Account</h1>
+							<p className="para">
+								Personal Information / Contact Details
+							</p>
+							<form className="form">
+								<Input
+									setnull={setName}
+									val={name}
+									change={handleChange1}
+									heading="Name"
+									placeholder="Enter your Name"
+									optional=""
+									type="text"
+								/>
+								<Input
+									setnull={setReg_no}
+									val={reg_no}
+									change={handleChange2}
+									heading="Registration Number"
+									optional=""
+									placeholder="Enter your Reg Number"
+									type="text"
+								/>
+								<Input
+									setnull={setEmail}
+									val={email}
+									change={handleChange3}
+									heading="VIT Email ID"
+									placeholder="Enter your VIT Email ID"
+									optional=""
+									type="email"
+								/>
+								<Input
+									setnull={setPh}
+									optional=""
+									val={ph}
+									change={handleChange4}
+									heading="Phone Number"
+									placeholder="Enter your Phone Number with Country Code"
+									type="tel"
+								/>
+							</form>
+							<Button
+								class="btn1"
+								ClickFunction={handleClick}
+								heading="Create an Account"
+							/>
+							<p className="bottom">
+								Already Have an Account?{" "}
+								<span
+									className="tologin"
+									onClick={() => {
+										navigate("/Login");
+									}}
+								>
+									{" "}
+									Login
+								</span>
+							</p>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default SignUp;
