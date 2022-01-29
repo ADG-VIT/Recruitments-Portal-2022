3dc65c1a3cd7bc0107b582c05fdefe0fea833c59
pranavvdesai <pranavdesai.psd@gmail.com>
2022-01-29 17:28:50 +0530

    feat: notistack

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { SnackbarProvider } from "notistack";
 
 ReactDOM.render(
+  <SnackbarProvider maxSnack={2}>
+
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    </React.StrictMode>
+    </SnackbarProvider>,
   document.getElementById("root")
 );
